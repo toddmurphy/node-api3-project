7a974ef15328783d3e85afef9af2bbe7e31b2fed
toddmurphy <toddmurphy93@gmail.com>
2020-01-15 17:10:56 -0400

    get all posts and get post by id working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,38 @@
 const express = require('express');
-
+const db = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // do your magic!
+  db.get()
+    .then(posts => {
+      res.status(200);
+      res.json(posts);
+    })
+    .catch(error => {
+      res.status(500);
+      res.json({ message: 'Sorry, no useres found on server', error });
+    });
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  const postsID = req.params.id;
+
+  db.getById(postsID)
+    .then(post => {
+      if (postsID) {
+        res.status(200);
+        res.json(post);
+      } else {
+        res.status(404);
+        res.json({ error: 'Sorry, no post with that ID. ' });
+      }
+    })
+    .catch(error => {
+      res.status(500);
+      res.json({ errorMessage: 'Sorry, no post returned by that ID from server', error });
+    });
 });
 
 router.delete('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const expressRouter = require('./posts/postRouter');
 const server = express();
+
+server.use('/api/posts', expressRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
